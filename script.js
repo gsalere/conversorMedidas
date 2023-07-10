b1b359351c1136fc68246e85701b479cc8f3ded2
Gabriel Salere Brandine <105891600+gsalere@users.noreply.github.com>
2023-07-10 16:27:44 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+function converter(){
+    let n1 = parseFloat(document.getElementById('n1').value)
+    let res = document.getElementById('resposta')
+    let calculo
+
+    const opcao = document.querySelector('input[name=medida]:checked').value;
+    
+   
+
+    switch (Number(opcao)){
+        case 1:
+            calculo = n1 *1000
+            res.innerHTML = `${n1} metros equivale a ${calculo} mm`
+            break
+        case 2:
+            calculo = n1*100
+            res.innerHTML = `${n1} metros equivale a ${calculo} cm`
+            break
+        case 3:
+            calculo = n1*10
+            res.innerHTML = `${n1} metros equivale a ${calculo} dm`
+            break
+        case 4:
+            calculo = n1/10
+            res.innerHTML = `${n1} metros equivale a ${calculo} dam`
+            break
+        case 5:
+            calculo = n1/100
+            res.innerHTML = `${n1} metros equivale a ${calculo} hm`
+            break
+        case 6:
+            calculo = n1/1000
+            res.innerHTML = `${n1} metros equivale a ${calculo} km`
+            break
+        default:
+            alert("Por favor, selecione uma unidade de medida!")
+            break
+    }
+    
+
+
+}
